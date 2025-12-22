19aae1295e510d432db61fbf0637efb7dae95bce
Nick Schneider <nickschneider101@gmail.com>
2025-12-21 23:45:17 -0800

    Update Prettier configuration with import sorting
    - Add no semicolons rule
    - Configure LF line endings and bracket spacing
    - Add import order plugin with custom sorting rules
    - Organize imports: React first, third-party, components, assets, data, hooks, styles

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@
  * Prettier will read RC files, you can set your prettier rules here
  */
 
-const baseLintingPath = '{,!(node_modules|dist|build|coverage|.git|reports)/**/}';
+const baseLintingPath = '{,!(node_modules|dist|build|coverage|.git|reports)/**/}'
 module.exports = {
   overrides: [
     {
@@ -36,4 +36,24 @@ module.exports = {
   printWidth: 140,
   tabWidth: 2,
   proseWrap: 'always',
-};
+  semi: false,
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  bracketSameLine: false,
+  endOfLine: 'lf',
+  quoteProps: 'as-needed',
+  htmlWhitespaceSensitivity: 'css',
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  importOrder: [
+    '^react$',
+    '<THIRD_PARTY_MODULES>',
+    '^components/(.*)$',
+    '^assets/(.*)$',
+    '^data/(.*)$',
+    '^hooks/(.*)$',
+    '^styles/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+}
