6ed7122f3289722cf8a614b9203edf26733d93da
Nick Schneider <nickschneider101@gmail.com>
2025-12-31 11:45:25 -0800

    feat: add new utility functions
    - Add technologiesUtils for technology-related helper functions
    - Add viewTransitionsUtils for view transition helpers

---
+++ src/utils/technologiesUtils.ts
@@ -0,0 +1,14 @@
+import { Technology } from 'types/technology.types'
+
+import { technologiesMap } from 'data/technologies'
+
+/**
+ * Maps technology names to their full Technology objects from the technologies map.
+ * Filters out any names not found in the map.
+ *
+ * @param technologyNames - Array of technology name strings to look up
+ * @returns Array of Technology objects, excluding any undefined entries
+ */
+export function getTechnologies(technologyNames: string[]): Technology[] {
+  return technologyNames.map((name) => technologiesMap.get(name)).filter((tech): tech is Technology => tech !== undefined)
+}
---
+++ src/utils/viewTransitionsUtils.ts
@@ -0,0 +1,7 @@
+/**
+ * Check if the View Transitions API is supported in the current browser.
+ * @returns true if document.startViewTransition is available as a function
+ */
+export function supportsViewTransitions(): boolean {
+  return typeof document.startViewTransition === 'function'
+}
