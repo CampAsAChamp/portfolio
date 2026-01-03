4864948aabe899b532940add4b63d7e587cf259e
Nick Schneider <nickschneider101@gmail.com>
2026-01-02 21:43:30 -0800

    feat: add entrance animation to hamburger menu on mobile
    - Added fadeInRight animation from animate.css to hamburger menu
    - Configured 0.6s duration with 0.3s delay for smooth entrance
    - Animation only visible on mobile devices (screens < 1100px)
    - Improves visual polish and user experience on mobile navigation

--- src/components/NavBar/HamburgerMenu.tsx
+++ src/components/NavBar/HamburgerMenu.tsx
@@ -9,7 +9,14 @@ export function HamburgerMenu({ navSlide }: HamburgerMenuProps): React.ReactElem
 
   return (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events
-    <div className="hamburger-menu" onClick={navSlide} {...keyboardProps} role="button" tabIndex={0} aria-label="Toggle navigation menu">
+    <div
+      className="hamburger-menu hamburger-entrance animate__animated animate__fadeInRight"
+      onClick={navSlide}
+      {...keyboardProps}
+      role="button"
+      tabIndex={0}
+      aria-label="Toggle navigation menu"
+    >
       <div className="hamburger-menu-line" id="hamburger-menu-line1" />
       <div className="hamburger-menu-line" id="hamburger-menu-line2" />
       <div className="hamburger-menu-line" id="hamburger-menu-line3" />
