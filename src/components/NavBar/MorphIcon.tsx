e22187e3063d5ed118c69a20143b059fc3b4c8c8
Nick Schneider <nickschneider101@gmail.com>
2026-07-29 18:20:45 -0700

    fix: scale theme icon same as navlinks

--- src/components/NavBar/MorphIcon.tsx
+++ src/components/NavBar/MorphIcon.tsx
@@ -6,8 +6,6 @@ export function MorphIcon({ isDarkMode }: MorphIconProps): React.ReactElement {
   return (
     <svg
       className={`morph-icon ${isDarkMode ? "moon-mode" : "sun-mode"}`}
-      width="24"
-      height="24"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
